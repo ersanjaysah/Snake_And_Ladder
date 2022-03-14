5517b9199d8f3f415ab64c95ef3ba485b7509aab
sanjay <ssah6694@gmail.com>
2022-03-14 15:57:52 +0530

    [Sanjay]Added:UC4 Winning position 100 files

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -7,14 +7,25 @@ namespace Snake_Ladder
         static void Main(string[] args)
         {
             int start = 0;
+            int finish = 100;
+            int rollDie = 0;
+            
             Random obj = new Random();
-            int randomDie = obj.Next(1, 7);
-            Console.WriteLine(randomDie);
 
-            string[] names = new string[] { "no play", "ladder", "snake" };
-            Random rand = new Random();
-            int index = rand.Next(names.Length);
-            Console.WriteLine(index);
+            while (start < finish)
+            {
+                int randomDie = obj.Next(1, 7);
+                Console.WriteLine(randomDie);
+                rollDie++;
+
+                Console.WriteLine(rollDie);
+
+                string[] names = new string[] { "no play", "ladder", "snake" };
+                Random rand = new Random();
+                int index = rand.Next(names.Length);
+                start++;
+            }
+           
         }
     }
 }
